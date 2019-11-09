1be507c1e84ad69452ed6c237ec711bbe8b67b8f
Jamie Thompson <j.thomperoo@hotmail.com>
2019-11-09 15:09:34 +0000

    Tidy shell tests (#54)

--- shell/shell_test.go
+++ shell/shell_test.go
@@ -18,6 +18,7 @@ limitations under the License.
 package shell_test
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -26,154 +27,183 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/jthomperoo/custom-pod-autoscaler/cpatest"
 	"github.com/jthomperoo/custom-pod-autoscaler/shell"
 )
 
-const (
-	testPipeValue = "test pipe value"
-	testStdout    = "test std out"
-	testStderr    = "test std err"
-	testCommand   = "command"
-	testTimeout   = 100
-)
+type command func(name string, arg ...string) *exec.Cmd
 
-func TestExecWithValuePipe_Success(t *testing.T) {
-	s := &shell.ExecuteWithPipe{Command: fakeExecCommandSuccess}
-	stdout, err := s.ExecuteWithPipe(testCommand, testPipeValue, testTimeout)
-	if err != nil {
-		t.Error(stdout.String())
-		t.Error(err)
-		return
+type process func(t *testing.T)
+
+type test struct {
+	description   string
+	expectedErr   error
+	expected      *string
+	commandString string
+	value         string
+	timeout       int
+	command       command
+}
+
+var tests []test
+
+var processes map[string]process
+
+func TestMain(m *testing.M) {
+	processes = map[string]process{}
+	tests = []test{
+		{
+			"Successful shell command",
+			nil,
+			stringPointer("test std out"),
+			"command",
+			"pipe value",
+			100,
+			fakeExecCommand("success", func(t *testing.T) {
+				// Check provided values are correct
+				// Due to the fake shell command, the actual command and value piped to it
+				// are arguments passed to this command - at argument position 6
+				// e.g. echo 'stdin' | command
+				commandAndPipe := os.Args[6]
+
+				stdin := strings.TrimSpace(strings.Split(commandAndPipe, "|")[0])
+				command := strings.TrimSpace(strings.Split(commandAndPipe, "|")[1])
+
+				// stdin is echoed and piped to the command, so the argument will be surrounded
+				// by an echo command
+				testPipeValueWithEcho := fmt.Sprintf("echo '%s'", "pipe value")
+
+				// Check command is correct
+				if !cmp.Equal(command, "command") {
+					fmt.Fprintf(os.Stderr, "stdin mismatch (-want +got):\n%s", cmp.Diff("command", command))
+					os.Exit(1)
+				}
+
+				// Check piped value in is correct
+				if !cmp.Equal(stdin, testPipeValueWithEcho) {
+					fmt.Fprintf(os.Stderr, "stdin mismatch (-want +got):\n%s", cmp.Diff(testPipeValueWithEcho, stdin))
+					os.Exit(1)
+				}
+
+				fmt.Fprint(os.Stdout, "test std out")
+				os.Exit(0)
+			}),
+		},
+		{
+			"Failed shell command",
+			errors.New("exit status 1"),
+			stringPointer("shell command failed"),
+			"command",
+			"pipe value",
+			100,
+			fakeExecCommand("failed", func(t *testing.T) {
+				fmt.Fprint(os.Stderr, "shell command failed")
+				os.Exit(1)
+			}),
+		},
+		{
+			"Failed shell command timeout",
+			errors.New("Command command timed out"),
+			nil,
+			"command",
+			"pipe value",
+			1,
+			fakeExecCommand("timeout", func(t *testing.T) {
+				fmt.Fprint(os.Stdout, "test std out")
+				time.Sleep(10 * time.Millisecond)
+				os.Exit(0)
+			}),
+		},
+		{
+			"Failed shell command fail to start",
+			errors.New("exec: already started"),
+			nil,
+			"command",
+			"pipe value",
+			100,
+			fakeExecCommandAndStart("fail to start", func(t *testing.T) {
+				fmt.Fprint(os.Stdout, "test std out")
+				os.Exit(0)
+			}),
+		},
 	}
+	code := m.Run()
+	os.Exit(code)
+}
 
-	stdoutStr := stdout.String()
-	if !cmp.Equal(stdoutStr, testStdout) {
-		t.Errorf("stdout mismatch (-want +got):\n%s", cmp.Diff(testStdout, stdoutStr))
+func TestExecWithValuePipe(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			s := &shell.ExecuteWithPipe{test.command}
+			stdout, err := s.ExecuteWithPipe(test.commandString, test.value, test.timeout)
+			if err == nil || test.expectedErr == nil {
+				if !(err == nil && test.expectedErr == nil) {
+					t.Errorf(stdout.String())
+					t.Errorf("error mismatch (-want +got):\n%s", cmp.Diff(test.expectedErr, err, cpatest.EquateErrors()))
+					return
+				}
+			} else {
+				if err.Error() != test.expectedErr.Error() {
+					t.Errorf(stdout.String())
+					t.Errorf("error mismatch (-want +got):\n%s", cmp.Diff(test.expectedErr, err, cpatest.EquateErrors()))
+					return
+				}
+			}
+
+			if test.expected == nil && stdout == nil {
+				return
+			}
+
+			stdoutStr := stdout.String()
+			if !cmp.Equal(stdoutStr, *test.expected) {
+				t.Errorf("stdout mismatch (-want +got):\n%s", cmp.Diff(stdoutStr, *test.expected))
+			}
+		})
 	}
 }
 
-func TestExecWithValuePipe_Fail(t *testing.T) {
-	s := &shell.ExecuteWithPipe{Command: fakeExecCommandFailure}
-	stderr, err := s.ExecuteWithPipe(testCommand, testPipeValue, testTimeout)
-	if err == nil {
-		t.Errorf("Expected error due to shell command exiting with non-zero exit code")
-		return
-	}
-
-	stderrStr := stderr.String()
-	if !cmp.Equal(stderrStr, testStderr) {
-		t.Errorf("stderr mismatch (-want +got):\n%s", cmp.Diff(testStderr, stderrStr))
-	}
-}
-
-func TestExecWithValuePipe_Timeout(t *testing.T) {
-	s := &shell.ExecuteWithPipe{Command: fakeExecCommandTimeout}
-	stderr, err := s.ExecuteWithPipe(testCommand, testPipeValue, testTimeout)
-	if err == nil {
-		t.Errorf("Expected error due to shell command timing out")
-		return
-	}
-
-	if stderr != nil {
-		t.Errorf("Expected no stderr due to shell command timing out")
-	}
-}
-
-func TestExecWithValuePipe_StartFail(t *testing.T) {
-	s := &shell.ExecuteWithPipe{Command: fakeExecCommandStartFail}
-	stderr, err := s.ExecuteWithPipe(testCommand, testPipeValue, testTimeout)
-	if err == nil {
-		t.Errorf("Expected error due to shell failing to start")
-		return
-	}
-
-	if stderr != nil {
-		t.Errorf("Expected no stderr due to shell command failing to start")
-	}
-}
-
-// Test shell commands
-
-func TestShellProcessSuccess(t *testing.T) {
+func TestShellProcess(t *testing.T) {
 	if os.Getenv("GO_TEST_PROCESS") != "1" {
 		return
 	}
 
-	// Due to the fake shell command, the actual command and value piped to it
-	// are arguments passed to this command - at argument position 5
-	// e.g. echo 'stdin' | command
-	commandAndPipe := os.Args[5]
+	processName := strings.Split(os.Args[3], "=")[1]
+	process := processes[processName]
 
-	stdin := strings.TrimSpace(strings.Split(commandAndPipe, "|")[0])
-	command := strings.TrimSpace(strings.Split(commandAndPipe, "|")[1])
-
-	// stdin is echoed and piped to the command, so the argument will be surrounded
-	// by an echo command
-	testPipeValueWithEcho := fmt.Sprintf("echo '%s'", testPipeValue)
-
-	// Check command is correct
-	if !cmp.Equal(command, testCommand) {
-		fmt.Fprintf(os.Stderr, "stdin mismatch (-want +got):\n%s", cmp.Diff(testCommand, command))
+	if process == nil {
+		t.Errorf("Process %s not found", processName)
 		os.Exit(1)
 	}
 
-	// Check piped value in is correct
-	if !cmp.Equal(stdin, testPipeValueWithEcho) {
-		fmt.Fprintf(os.Stderr, "stdin mismatch (-want +got):\n%s", cmp.Diff(testPipeValueWithEcho, stdin))
-		os.Exit(1)
-	}
+	process(t)
 
-	fmt.Fprint(os.Stdout, testStdout)
-	os.Exit(0)
-}
-
-func TestShellProcessFail(t *testing.T) {
-	if os.Getenv("GO_TEST_PROCESS") != "1" {
-		return
-	}
-
-	fmt.Fprint(os.Stderr, testStderr)
+	// Process should call os.Exit itself, if not exit with error
 	os.Exit(1)
 }
 
-func TestShellProcessTimeout(t *testing.T) {
-	if os.Getenv("GO_TEST_PROCESS") != "1" {
-		return
+func fakeExecCommandAndStart(name string, process process) command {
+	processes[name] = process
+	return func(command string, args ...string) *exec.Cmd {
+		cs := []string{"-test.run=TestShellProcess", "--", fmt.Sprintf("-process=%s", name), command}
+		cs = append(cs, args...)
+		cmd := exec.Command(os.Args[0], cs...)
+		cmd.Env = []string{"GO_TEST_PROCESS=1"}
+		cmd.Start()
+		return cmd
 	}
-
-	time.Sleep(testTimeout * 2 * time.Millisecond)
-	os.Exit(0)
 }
 
-func fakeExecCommandSuccess(command string, args ...string) *exec.Cmd {
-	cs := []string{"-test.run=TestShellProcessSuccess", "--", command}
-	cs = append(cs, args...)
-	cmd := exec.Command(os.Args[0], cs...)
-	cmd.Env = []string{"GO_TEST_PROCESS=1"}
-	return cmd
+func fakeExecCommand(name string, process process) command {
+	processes[name] = process
+	return func(command string, args ...string) *exec.Cmd {
+		cs := []string{"-test.run=TestShellProcess", "--", fmt.Sprintf("-process=%s", name), command}
+		cs = append(cs, args...)
+		cmd := exec.Command(os.Args[0], cs...)
+		cmd.Env = []string{"GO_TEST_PROCESS=1"}
+		return cmd
+	}
 }
 
-func fakeExecCommandFailure(command string, args ...string) *exec.Cmd {
-	cs := []string{"-test.run=TestShellProcessFail", "--", command}
-	cs = append(cs, args...)
-	cmd := exec.Command(os.Args[0], cs...)
-	cmd.Env = []string{"GO_TEST_PROCESS=1"}
-	return cmd
-}
-
-func fakeExecCommandTimeout(command string, args ...string) *exec.Cmd {
-	cs := []string{"-test.run=TestShellProcessTimeout", "--", command}
-	cs = append(cs, args...)
-	cmd := exec.Command(os.Args[0], cs...)
-	cmd.Env = []string{"GO_TEST_PROCESS=1"}
-	return cmd
-}
-
-func fakeExecCommandStartFail(command string, args ...string) *exec.Cmd {
-	cs := []string{"-test.run=TestShellProcessSuccess", "--", command}
-	cs = append(cs, args...)
-	cmd := exec.Command(os.Args[0], cs...)
-	cmd.Env = []string{"GO_TEST_PROCESS=1"}
-	cmd.Start()
-	return cmd
+func stringPointer(value string) *string {
+	return &value
 }
